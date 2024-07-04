fff282ace73cecb8c4c52344bfb055ce547ec57d
sai Akhilesh <101054891+SaiAkhileshP@users.noreply.github.com>
2024-07-04 13:07:05 +0530

    Add files via upload

---
+++ image to text.py
@@ -0,0 +1,54 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+
+import google.generativeai as genai
+api = '<Gemini_Api_Key>'
+genai.configure(api_key=api)
+
+def upload_to_gemini(path, mime_type=None):
+  """Uploads the given file to Gemini.
+
+  See https://ai.google.dev/gemini-api/docs/prompting_with_media
+  """
+  file = genai.upload_file(path, mime_type=mime_type)
+  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+  return file
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 0.95,
+  "top_k": 32,
+  "max_output_tokens": 1024,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.0-pro-vision-latest",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+# TODO Make these files available on the local file system
+# You may need to update the file paths
+files = [
+  upload_to_gemini("<file location>", mime_type="image/png"),
+]
+
+response = model.generate_content([
+  files[0],
+  "extract the text from the image",
+  "Image: extract the text from the image",
+])
+
+print(response.text)
